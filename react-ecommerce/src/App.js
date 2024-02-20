dce66dd7879e6a1a969f05ff5cf5cbafbafe2d7b
Dhaval5353 <mractiongamer3158@gmail.com>
2024-02-21 01:03:38 +0530

    changes

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <div className="App">
-      <ProductList></ProductList>
+      <SignupPage></SignupPage>
     </div>
   );
 }
